d646406120d03d6692ea34ab5436917ab3bda7c8
Gh-Yeh <yehyaghady@gmail.com>
2026-09-18 20:01:54 +0300

    Security patch: Moved SECRET_KEY to hidden .env file

--- wehbe_backend/settings.py
+++ wehbe_backend/settings.py
@@ -11,21 +11,31 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from the hidden .env file
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "hdsajhdjs$$##djhsajdhjkh2312189sjad!!snadjasazx!qs"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["wehbegroup.pythonanywhere.com", "www.wehbegroup.pythonanywhere.com"]
+ALLOWED_HOSTS = [
+    "wehbegroup.pythonanywhere.com",
+    "www.wehbegroup.pythonanywhere.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
